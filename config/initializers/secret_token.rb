1a695442e92fb054ab5ec08d6a0a9f0ab2e8267a
Dave Newton <davelnewton@gmail.com>
2011-09-06 14:34:46 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageMagickExamples::Application.config.secret_token = '77481e5a8206e6504bcd58d416fbf8c299cae0240019dc24bf0cde0e57e5832e0c567f65c297510fbc09ebac8f45276720afb8a610082f02e5d6eebbc290ef2e'
